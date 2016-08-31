e36074c27d4ef1f663e0b3f125f595fc5cebd853
Fujiwara <dev.fsap@gmail.com>
2016-09-01 00:08:13 +0900

    ブログバナーリンク削除 SP版TOPのスタッフ紹介リンク削除

--- js/common.js
+++ js/common.js
@@ -6,10 +6,9 @@
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-72682127-1', 'auto');
-//  ga('create', 'UA-17535700-11', 'auto');  // まつもとクリニック本番用
+  ga('create', 'UA-83436171-1', 'auto');
   ga('send', 'pageview');
 
 /*!
